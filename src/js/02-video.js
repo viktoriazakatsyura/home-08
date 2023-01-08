fa6b262eb6adcfe94135a21eddbafe02230dd642
Viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2023-01-08 15:09:37 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,49 @@
 import Vimeo from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import Throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 
 const player = new Vimeo.Player(iframe);
 
-const onPlay = function (data) {
-  const strigifyData = JSON.stringify(data);
-  localStorage.setItem(STORAGE_KEY, strigifyData);
-};
-player.on('timeupdate', throttle(onPlay, 1000));
+setVideoTime();
 
-function resumePlayback() {
-  if (JSON.parse(localStorage.getItem(STORAGE_KEY)) === null) {
+player.on('timeupdate', Throttle(saveTimeVideo, 1000, { leading: false }));
+
+function saveTimeVideo(info) {
+  localStorage.setItem(STORAGE_KEY, info.seconds);
+}
+
+function setVideoTime() {
+  if (!localStorage.getItem(STORAGE_KEY)) {
     return;
   }
-  const paused = JSON.parse(localStorage.getItem(STORAGE_KEY))['seconds'];
-  if (paused) {
-    player
-      .setCurrentTime(paused)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'Error':
-            break;
-          default:
-            break;
-        }
-      });
-  }
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
 }
-resumePlayback();
+
+// const onPlay = function (data) {
+//   const strigifyData = JSON.stringify(data);
+//   localStorage.setItem(STORAGE_KEY, strigifyData);
+// };
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// function resumePlayback() {
+//   if (JSON.parse(localStorage.getItem(STORAGE_KEY)) === null) {
+//     return;
+//   }
+//   const paused = JSON.parse(localStorage.getItem(STORAGE_KEY))['seconds'];
+//   if (paused) {
+//     player
+//       .setCurrentTime(paused)
+//       .then(function (seconds) {})
+//       .catch(function (error) {
+//         switch (error.name) {
+//           case 'Error':
+//             break;
+//           default:
+//             break;
+//         }
+//       });
+//   }
+// }
+// resumePlayback();
